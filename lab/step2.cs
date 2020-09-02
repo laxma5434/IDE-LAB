d09533a756636aaad89a23a5f6f22f48d173b1a6
M.Laxmareddy <59173934+laxma5434@users.noreply.github.com>
2020-09-02 17:07:40 +0530

    Add files via upload

---
+++ lab/step2.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            String name = "laxma";
+            Console.WriteLine("Hello "+name);
+        }
+    }
+}
